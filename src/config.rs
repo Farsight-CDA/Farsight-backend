263d13c01832965f87896b85c4de03c23f345094
jojii <jojii@gmx.net>
2022-10-16 23:44:52 +0200

    remove todo

--- src/config.rs
+++ src/config.rs
@@ -9,7 +9,6 @@ use serde::{Deserialize, Serialize};
 
 use crate::types::contract::ContractType;
 
-// TODO: add constant: Bridge-base-gas: u64
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
     pub bridge_base_gas: u64,
